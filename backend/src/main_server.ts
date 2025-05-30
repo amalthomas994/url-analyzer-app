2c0635c1c624f2ceab292aea15ae4e5e275c701e
Amal Thomas <amalthomas994@gmail.com>
2025-05-30 15:12:21 -0400

    feature: Added code to fetch URL content via Axios

--- backend/src/main_server.ts
+++ backend/src/main_server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import axios from 'axios';
+import { url } from 'inspector';
 
 console.log("Starting Server")
 const app = express();
@@ -12,10 +14,11 @@ const PORT = process.env.PORT || 3000; //Server Listen port. Check env variable 
 app.get('/', (req: Request, res: Response)=> {
     console.log("Root Path Called");
     res.status(200).json({message: "Backed Server is Active!"});
+
 });
 
-//
-app.get('/analyze_url', (req: Request, res: Response) => {
+//Analyze Url Path. URL will be supplied via URL query string parameter
+app.get('/analyze_url', async (req: Request, res: Response) => {
     const url_to_analyze = req.query.url as string | undefined;
 
     if(!url_to_analyze){
@@ -24,13 +27,55 @@ app.get('/analyze_url', (req: Request, res: Response) => {
         return;
     }
 
-    console.log(`Analyze URL: ${url_to_analyze}`);
+    console.log(`Attempting to fetch URL: ${url_to_analyze}`);
 
-    res.status(200).json({
-        message: 'URL Analysis Request Received',
-        requestedUrl: url_to_analyze
-    });
-    return;
+    try {
+        //Using axios to retrive HTML content of supplied URL to analyze
+        const response = await axios.get(url_to_analyze, {
+            headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'},
+            timeout: 10000
+        });
+
+        //Return snippet of HTML
+        const html_snippet = typeof response.data === 'string' ? response.data.substring(0, 500) : "Could not retrive HTML as string";
+        console.log(`Successfully reterived URL: ${url_to_analyze}. Status: [${response.status}]`)
+
+        res.status(200).json({
+            message: `URL retrived successfully!`,
+            requestedURL: url_to_analyze,
+            html_snippet: html_snippet + '...'
+        })
+    } catch (error: any) {
+        console.error(`Error retrieving URL ${url_to_analyze}:`, error.message)
+
+        if (axios.isAxiosError(error)){ //Check if error is an Axios error
+            if (error.response){ //If server responded with a status code
+                res.status(error.response.status).json({
+                    message: `Server responded with the status ${error.response.status}`,
+                    requestedURL: url_to_analyze,
+                    error: "Failed to retrieve URL!"
+                })
+            } else if (error.request){ //No response from server
+            res.status(500).json({
+                message: `No response from server: ${error.message}`,
+                requestedURL: url_to_analyze,
+                error: "Failed to retrieve URL!"
+            })} else {
+            res.status(500).json({
+                message: `ERROR: ${error.message}`,
+                requestedURL: url_to_analyze,
+                error: "Failed to retrieve URL!"
+            })
+            }
+        } else { //Non axios errors
+            res.status(500).json({
+                message: error.message,
+                requestedURL: url_to_analyze,
+                error: "Unexpected Error!"
+            })
+        }
+        return;
+    }
 });
 
 
