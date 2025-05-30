b2bcbb29d70442feb7f264c2f32d28d3269169de
Amal Thomas <amalthomas994@gmail.com>
2025-05-30 13:41:32 -0400

    feature: Added basic Express backend server logic with Typescript and placeholder routes

--- backend/src/main_server.ts
+++ backend/src/main_server.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
+console.log("Starting Server")
 const app = express();
 app.use(cors()); //Enable CORS
 app.use(express.json()); //Parse incoming requests as JSON payloads
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000; //Server Listen port. Check env variable 
 //Root Path
 app.get('/', (req: Request, res: Response)=> {
     console.log("Root Path Called");
-    res.status(200).json({message: "Server Active!"});
+    res.status(200).json({message: "Backed Server is Active!"});
 });
 
 //
@@ -18,7 +19,9 @@ app.get('/analyze_url', (req: Request, res: Response) => {
     const url_to_analyze = req.query.url as string | undefined;
 
     if(!url_to_analyze){
+        console.log("URL Query Parameter not passed!")
         res.status(400).json({ error: "URL Query parameter required!"});
+        return;
     }
 
     console.log(`Analyze URL: ${url_to_analyze}`);
@@ -27,4 +30,15 @@ app.get('/analyze_url', (req: Request, res: Response) => {
         message: 'URL Analysis Request Received',
         requestedUrl: url_to_analyze
     });
+    return;
+});
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong on the server!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend server is listening on http://localhost:${PORT}`);
 });
